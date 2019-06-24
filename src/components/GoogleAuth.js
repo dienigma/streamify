b07c861a443fb85b9dd7a08bc58b0a0ba24c8e38
dienigma <joshi.chinmay09@gmail.com>
2019-06-24 22:58:47 +0530

    Built OAuth and put redux boilerplate

---
+++ src/components/GoogleAuth.js
@@ -0,0 +1,58 @@
+import React from "react";
+
+class GoogleAuth extends React.Component {
+  state = {
+    isSignedIn: null
+  };
+  componentDidMount() {
+    window.gapi.load("client:auth2", () => {
+      window.gapi.client
+        .init({
+          clientId:
+            "208923110017-gq8sghu066t5t99kpcferblhp8cnck18.apps.googleusercontent.com",
+          scope: "email"
+        })
+        .then(() => {
+          this.auth = window.gapi.auth2.getAuthInstance();
+          this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+          this.auth.isSignedIn.listen(this.onAuthChange);
+        });
+    });
+  }
+  onAuthChange = () => {
+    this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+  };
+
+  onSignInClick = () => {
+    this.auth.signIn();
+  };
+
+  onSignOutClick = () => {
+    this.auth.signOut();
+  };
+
+  rednerAuthButton() {
+    if (this.state.isSignedIn === null) {
+      return null;
+    } else if (this.state.isSignedIn) {
+      return (
+        <button onClick={this.onSignOutClick} className="ui red google button">
+          <i className="google icon" />
+          Sign Out
+        </button>
+      );
+    } else {
+      return (
+        <button onClick={this.onSignInClick} className="ui red google button">
+          <i className="google icon" />
+          Sign in with Google
+        </button>
+      );
+    }
+  }
+  render() {
+    return <div>{this.rednerAuthButton()}</div>;
+  }
+}
+
+export default GoogleAuth;
